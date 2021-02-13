87567fd565899485f816931dcc3ed7708d9c0725
terrancewang <terrance.3512@gmail.com>
2021-02-12 20:31:36 -0800

    added labels to sliders

--- sketch.js
+++ sketch.js
@@ -1,8 +1,19 @@
+// max speed of boid
 let max_speed = 5;
+// size of boid
 let size = 4;
+// neighbors radius
 let nearby = 70;
+// framerate
 let fr = 30
+
 let slider;
+let counter;
+
+let separationSlider = 50;
+let alignmentSlider = 50;
+let cohesionSlider = 50;
+
 
 // p5js code
 
@@ -12,49 +23,65 @@ function setup() {
   var height = canvasDiv.offsetHeight - 10
   var canvas = createCanvas(width, height);
   canvas.parent('sketch-holder');
+  canvas.mouseClicked(canvasClick);
 
+  labelSeparation = createDiv('Separation: ' + separationSlider);
+  labelSeparation.parent('separation');
   sliderSeparation = createSlider(0, 100, 50);
-  sliderSeparation.parent('slider-holder')
+  sliderSeparation.parent('separation');
   sliderSeparation.style('width', '80px');
   sliderSeparation.addClass('slider');
 
+  labelAlignment = createDiv('Alignment: ' + alignmentSlider);
+  labelAlignment.parent('alignment');
   sliderAlignment = createSlider(0, 100, 50);
-  sliderAlignment.parent('slider-holder')
+  sliderAlignment.parent('alignment');
   sliderAlignment.style('width', '80px');
   sliderAlignment.addClass('slider');
 
+  labelCohesion = createDiv('Cohesion: ' + cohesionSlider);
+  labelCohesion.parent('cohesion');
   sliderCohesion = createSlider(0, 100, 50);
-  sliderCohesion.parent('slider-holder')
+  sliderCohesion.parent('cohesion');
   sliderCohesion.style('width', '80px');
   sliderCohesion.addClass('slider');
+
   
   flock = new Flock();
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 1; i++) {
     var x = random(20, width - 20);
     var y = random(20, height - 20);
     flock.addBoid(x, y);
   }
-  frameRate(30)
+  counter = createDiv('number of boids: ' + flock.boids.length);
+  counter.parent('counter-holder');
 }
 
-function mouseClicked() {
-  flock.addBoid(mouseX, mouseY);
-  return false;
+function canvasClick() {
+  if (flock.boids.length < 125) {
+    flock.addBoid(mouseX, mouseY);
+    counter.html('number of boids: ' + flock.boids.length);
+    counter.parent('counter-holder');
+  }
 }
 
 function draw() {
+  frameRate(fr);
   background(255);
+  separationSlider = sliderSeparation.value()
+  labelSeparation.html('Separation: ' + separationSlider)
+  alignmentSlider = sliderAlignment.value()
+  labelAlignment.html('Alignment: ' + alignmentSlider)
+  cohesionSlider = sliderCohesion.value()
+  labelCohesion.html('Cohesion: ' + cohesionSlider)
   flock.update();
-  //print(frameRate())
+  //print(Math.round(frameRate()));
 }
 
 function windowResized() {
   var canvasDiv = document.getElementById('sketch-holder')
   var width = canvasDiv.offsetWidth - 10
   var height = canvasDiv.offsetHeight - 10
-  print(width)
-  print(height)
-  print('resize')
   resizeCanvas(width, height);
 }
 
@@ -83,11 +110,12 @@ class Flock {
       var avg = createVector(0, 0)
 
       // separation scaling factor
-      var x = 0.4
+      var separationScale = 0.4 * separationSlider / 50
       // alignment scaling factor
-      var y = 0.4
+      var alignmentScale = 0.4 * alignmentSlider / 50
       // cohesion scaling factor
-      var z = 0.4
+      var cohesionScale = 0.4 * cohesionSlider / 50
+
       for (var n = 0; n < near.length; n++) {  
         // separation
         var force = p5.Vector.sub(this.boids[i].pos, near[n].pos)
@@ -95,7 +123,6 @@ class Flock {
         separ.add(force)
 
         //alignment    
-
         angle.add(near[n].velocity)
 
         //cohesion 
@@ -103,13 +130,13 @@ class Flock {
       }
 
       separ.normalize()
-      separ.mult(x)
+      separ.mult(separationScale)
       this.boids[i].acceleration.add(separ)
 
       if (near.length != 0) {
         angle.div(near.length)
         angle.normalize()
-        angle.mult(y)
+        angle.mult(alignmentScale)
         this.boids[i].acceleration.add(angle)
       }
 
@@ -118,7 +145,7 @@ class Flock {
         avg.div(near.length)
         var cohes = p5.Vector.sub(avg, this.boids[i].pos)
         cohes.normalize()
-        cohes.mult(z)
+        cohes.mult(cohesionScale)
         this.boids[i].acceleration.add(cohes)
       }
       this.boids[i].next()
@@ -160,7 +187,6 @@ class Boid {
     if (this.pos.y < 0) {this.pos.y = height;}
 
     this.acceleration = p5.Vector.div(this.velocity, 2)
-
   }
 
   render() {
